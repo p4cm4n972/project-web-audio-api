e049a49fe4603a9dff94b5564a79f7d82ab95d77
p4cm4n972 <manuel.adele@gmail.com>
2017-12-07 17:59:03 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# project-web-audio-api
---
+++ audio.js
@@ -0,0 +1,46 @@
+var contexteAudio = new AudioContext();
+var analyseur = contexteAudio.createAnalyser();
+var myAudio = document.querySelector('audio');
+
+var source = contexteAudio.createMediaElementSource(myAudio);
+source.connect(analyseur);
+analyseur.connect(contexteAudio.destination);
+
+analyseur.fftSize = 2048;
+var tailleMemoireTampon = analyseur.frequencyBinCount;
+var tableauDonnees = new Uint8Array(tailleMemoireTampon);
+analyseur.getByteTimeDomainData(tableauDonnees);
+
+var canvas = document.getElementById('canvas');
+var contexteCanvas = canvas.getContext('2d');
+contexteCanvas.clearRect(0, 0, 600, 300);
+
+function dessiner() {
+    console.log('dessin');
+    dessin = requestAnimationFrame(dessiner);
+    analyseur.getByteTimeDomainData(tableauDonnees);
+    console.log(tableauDonnees);
+    contexteCanvas.fillStyle = 'rgb(200, 200, 200)';
+    contexteCanvas.fillRect(0, 0, 600, 300);
+    var largeurSegment = 600 * 1.0 / tailleMemoireTampon;
+    var x = 0;
+    for(var i = 0; i < tailleMemoireTampon; i++) {
+        
+             var v = tableauDonnees[i] / 128.0;
+             var y = v * 300/2;
+     
+             if(i === 0) {
+               contexteCanvas.moveTo(x, y);
+             } else {
+               contexteCanvas.lineTo(x, y);
+             }
+     
+             x += largeurSegment;
+           }
+           contexteCanvas.lineTo(canvas.width, canvas.height/2);
+           contexteCanvas.stroke();
+           dessiner();
+        };
+
+
+
