6b3349ea589676899985edb82ce5f672caf0e45f
p4cm4n972 <manuel.adele@gmail.com>
2017-12-08 17:21:08 +0100

    BACK UP

--- audio-final.js
+++ audio-final.js
@@ -14,18 +14,20 @@ analyseurB.fftSize = 256;
 var tailleMemoireTamponB = analyseurB.frequencyBinCount;
 var tailleMemoireTamponF = analyseurF.frequencyBinCount;
 
+
 var tableauDonneesB = new Uint8Array(tailleMemoireTamponB);
 var tableauDonneesF = new Uint8Array(tailleMemoireTamponF);
 
 analyseurF.getByteTimeDomainData(tableauDonneesF);
+console.log(tableauDonneesF);
 
 var canvasF = document.getElementById('canvas');
 var contexteCanvasF = canvasF.getContext('2d');
 contexteCanvasF.clearRect(0, 0, 300, 150);
 
 function frequence() {
-    dessin = requestAnimationFrame(frequence);
-    analyseurF.getByteTimeDomainData(tableauDonneesF);
+  dessin = requestAnimationFrame(frequence);
+  analyseurF.getByteTimeDomainData(tableauDonneesF);
     contexteCanvasF.fillStyle = 'rgb(200, 200, 200)';
     contexteCanvasF.fillRect(0, 0, 300, 150);
     contexteCanvasF.lineWidth = 2;
